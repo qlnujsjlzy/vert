0dab928ecfa6e92900df9f641ed6295cffff8229
qlnujsjlzy <lzhy719@163.com>
2018-05-10 01:06:46 +0800

    init mybatis-plus

---
+++ vert-admin/src/main/java/com/zhaoyang/vert/core/datascope/DataScope.java
@@ -0,0 +1,27 @@
+package com.zhaoyang.vert.core.datascope;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author : zhaoyang.li
+ * @date : 2018/5/10
+ */
+@Setter
+@Getter
+public class DataScope {
+
+    /**
+     * 限制范围的字段名称
+     */
+    private String scopeName = "dept_Id";
+
+    /**
+     * 具体的数据范围
+     */
+    private List<Integer> deptIds;
+
+
+}
